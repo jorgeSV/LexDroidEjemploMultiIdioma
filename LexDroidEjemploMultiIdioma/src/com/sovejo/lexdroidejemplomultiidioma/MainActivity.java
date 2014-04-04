afd45d8e8db5ba3de37d77b5029df48c5ea279b4
jorgeSV <amk114@gmail.com>
2014-04-04 13:34:26 +0200

    Ejercicio completado

--- LexDroidEjemploMultiIdioma/src/com/sovejo/lexdroidejemplomultiidioma/MainActivity.java
+++ LexDroidEjemploMultiIdioma/src/com/sovejo/lexdroidejemplomultiidioma/MainActivity.java
@@ -1,22 +1,20 @@
 package com.sovejo.lexdroidejemplomultiidioma;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity 
+{
+	TextView textView01;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
+		textView01 = (TextView) findViewById(R.id.textView01); 
 	}
 
 }
